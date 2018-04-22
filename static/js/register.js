9d9ca1bbc3dd766baa2f11be2106f1e93ba5cdc3
Angis <Aengis5195@googlemail.com>
2018-04-22 14:49:48 +0200

    register.js update

--- static/js/register.js
+++ static/js/register.js
@@ -1,7 +1,66 @@
-function openRegisterModal(){
-    console.log("Hallo aus Register.js - openRegisterModal")
-  showRegisterForm();
-  setTimeout(function(){
-    $('#loginModal').modal('show');
-  }, 230);
+function showRegisterForm() {
+	$('.loginBox').fadeOut('fast', function() {
+		$('.registerBox').fadeIn('fast');
+		$('.login-footer').fadeOut('fast', function() {
+			$('.register-footer').fadeIn('fast');
+		});
+		$('.modal-title').html('Register');
+	});
+	$('.error')
+		.removeClass('alert alert-danger')
+		.html('');
+}
+function showLoginForm() {
+	$('#loginModal .registerBox').fadeOut('fast', function() {
+		$('.loginBox').fadeIn('fast');
+		$('.register-footer').fadeOut('fast', function() {
+			$('.login-footer').fadeIn('fast');
+		});
+
+		$('.modal-title').html('Login');
+	});
+	$('.error')
+		.removeClass('alert alert-danger')
+		.html('');
+}
+
+function openRegisterModal() {
+	console.log('Hallo aus Register.js - openRegisterModal');
+	showRegisterForm();
+	setTimeout(function() {
+		$('#loginModal').modal('show');
+	}, 230);
+}
+
+function openLoginModal() {
+	showLoginForm();
+	setTimeout(function() {
+		$('#loginModal').modal('show');
+	}, 230);
+}
+
+function loginAjax() {
+	/*   Remove this comments when moving to server
+    $.post( "/login", function( data ) {
+            if(data == 1){
+                window.location.replace("/home");            
+            } else {
+                 shakeModal(); 
+            }
+        });
+        */
+
+	/*   Simulate error message from the server   */
+	shakeModal();
+}
+
+function shakeModal() {
+	$('#loginModal .modal-dialog').addClass('shake');
+	$('.error')
+		.addClass('alert alert-danger')
+		.html('Invalid email/password combination');
+	$('input[type="password"]').val('');
+	setTimeout(function() {
+		$('#loginModal .modal-dialog').removeClass('shake');
+	}, 1000);
 }
